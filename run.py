0c661703669b7992e39ce64ac08ff5ff1a9cc3dd
satry22 <sara.tryzell@gmail.com>
2022-01-02 14:35:12 +0000

    add basic structure and defining variables

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+HIDDEN_BOARD = [[' ']] * 6 for x in range(6)]
+GUESS_BOARD = [[' ']] * 6 for x in range(6)]
+
+letters_to_numbers = {'A':0, 'B':1, 'C':2, 'D':3, 'E':4, 'F':5}
+
+def print_board(board):
+    print('  A B C D E F')
+    print('  -----------')
+    row_number = 1
+    for row in board:
+        print('%d|%s|' % (row_number, '|'.join(row)))
+        row_number += 1
+
+def create_ships(board):
+    for ship in range(4):
+        ship_row, ship_column = randint(0,5), randint(0,5)
+        while board[ship_row][ship_random] == 'X':
+            ship_row, ship_column = randint(0,5), randint(0,5)
+            
+
+
+def get_ship_location():
+    pass
+
+def count_hit_ships():
+    pass
+
+
